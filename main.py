d01ae8ca34e05780dbf9aa60cefbeeb2a22fd258
kitakou0313 <kitakou0313@gmail.com>
2023-01-24 05:06:38 +0000

    Fix code to use format string

--- main.py
+++ main.py
@@ -9,7 +9,10 @@ if __name__ == "__main__":
     input_image_paths = glob.glob(INPUTS_IMAGE_DIR+"/*")
 
     for image_path in input_image_paths:
-        file_name = os.path.split(image_path)[1]
+        parent_dir, file_name = os.path.split(image_path)
+
         image = Image.open(image_path)
         image = image.convert('RGB')
-        image.save(OUTPUTS_IMAGE_DIR+"/"+file_name.split(".")[0]+".eps")
+
+        file_name_without_extention = file_name.split(".")[0]
+        image.save(f"{OUTPUTS_IMAGE_DIR}/{file_name_without_extention}.eps")
