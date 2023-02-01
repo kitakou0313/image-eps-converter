aff0710f53039dddc8451098f96f78a77757ddc6
kitakou0313 <kitakou0313@gmail.com>
2023-02-01 07:01:47 +0000

    Fix csv exporter

--- csv-exporter.py
+++ csv-exporter.py
@@ -1,7 +1,9 @@
 from tensorboard.backend.event_processing.event_accumulator import EventAccumulator
+import csv
+import sys
 
 if __name__ == "__main__":
-    LOG_PATH = 'datas/graph-datas/uncased/fixed-for-chap-7/logs/events.out.tfevents.1674874989.e69379b7ec54.59.0'
+    LOG_PATH = 'datas/graph-datas/uncased/sampled-for-ntgaed-clean-mlp/logs/events.out.tfevents.1675232493.f7efdf6531e5.66.0'
 
     event_acc = EventAccumulator(LOG_PATH, size_guidance={'scalars': 0})
     event_acc.Reload()  # ログファイルのサイズによっては非常に時間がかかる
@@ -11,15 +13,18 @@ if __name__ == "__main__":
         events = event_acc.Scalars(tag)
         scalars[tag] = [event.value for event in events]
 
-    train_acc = {"Value": [], "Step": []}
-    test_acc = {"Value": [], "Step": []}
+    train_acc: list[dict] = []
+    test_acc: list[dict] = []
 
     for ind, acc in enumerate(scalars["Train/acc"]):
-        train_acc["Step"].append(ind+1)
-        train_acc["Value"].append(acc)
+        train_acc.append({"Value": acc, "Step": ind+1})
 
     for ind, acc in enumerate(scalars["Validation/acc"]):
-        test_acc["Step"].append(ind+1)
-        test_acc["Value"].append(acc)
+        test_acc.append({"Value": acc, "Step": ind+1})
 
-    pass
+    writer = csv.DictWriter(sys.stdout, train_acc[0].keys())
+    print("Train")
+    writer.writerows(train_acc)
+
+    print("Test")
+    writer.writerows(test_acc)
